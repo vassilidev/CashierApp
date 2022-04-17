5397ebff6e9e0e79bd42487df5cc63bfa60ff028
vassilidev <vassilidevnet@gmail.com>
2022-04-17 15:17:06 +0200

    Create UserController.php with actions views + requests

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index()
+    {
+        return view('pages.users.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param UserRequest $request
+     * @return RedirectResponse
+     */
+    public function store(UserRequest $request)
+    {
+        User::create($request->validated());
+
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return Application|Factory|View
+     */
+    public function show(User $user)
+    {
+        return view('pages.users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param User $user
+     * @return Application|Factory|View
+     */
+    public function edit(User $user)
+    {
+        return view('pages.users.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UserRequest $request
+     * @param User $user
+     * @return RedirectResponse
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->validated());
+
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param User $user
+     * @return RedirectResponse
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect()->back();
+    }
+}
